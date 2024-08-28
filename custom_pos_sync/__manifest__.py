9db2b170a5815d585f373711a2b5e3e08f0675a9
Umar <maniar7@gmail.com>
2024-08-28 16:53:15 +0530

    [Added] added dependsin manifest

--- custom_pos_sync/__manifest__.py
+++ custom_pos_sync/__manifest__.py
@@ -8,7 +8,7 @@
     'description': """ This Module is create a external apis for leaf balance and sync the customer from odoo to endpoint
 """,
     'category': 'Point Of Sale',
-    'depends': ['web','portal'],
+    'depends': ['web','portal','point_of_sale','contacts'],
     'data': [
             'security/ir.model.access.csv',
             'views/generate_token.xml'
