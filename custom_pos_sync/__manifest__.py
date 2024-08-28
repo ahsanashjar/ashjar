31658825f6db640a6c30d6666db180e8d7110f44
Umar <maniar7@gmail.com>
2024-08-28 16:44:39 +0530

    [Update] manifest Version

--- custom_pos_sync/__manifest__.py
+++ custom_pos_sync/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'Customer & POS API',
-    'version': '1.0',
+    'version': '1.1',
     'summary': 'Sync Customer,Leaf Balance',
     'author': "~Areterix Technologies LLP",
     'website': "https://areterix.com",
