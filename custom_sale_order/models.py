514f5d97e2cb500d2eaf4176db6537860f4a23fa
Areterix <mmalek@areterix.com>
2025-09-03 09:39:58 +0530

    UPDATE FUNCTION

--- custom_sale_order/models.py
+++ custom_sale_order/models.py
@@ -442,6 +442,64 @@ class StockPicking(models.Model):
             "products": []
         }
 
+        # for bom in kit_boms:
+        #     available_qty = float('inf')
+        #
+        #     for line in bom.bom_line_ids:
+        #         component = line.product_id
+        #         component_qty_needed = line.product_qty
+        #
+        #         component_qty_available = self.env['stock.quant']._get_available_quantity(component, stock_location)
+        #         kits_possible_with_component = component_qty_available / component_qty_needed if component_qty_needed else 0
+        #
+        #         available_qty = min(available_qty, kits_possible_with_component)
+        #
+        #     product_data = {
+        #         "product_id": bom.product_id.default_code,
+        #         "new_stock_qty": float(available_qty)
+        #     }
+        #     warehouse_data["products"].append(product_data)
+        #
+        # location_stock_data = [warehouse_data]
+        # # _logger.info('location_stock_data: %s', location_stock_data)
+        products = self.env['product.product'].with_context(location=stock_location.id).search([])
+
+        for product in products:
+            product_data = {
+                "product_id": product.default_code or product.id,
+                "new_stock_qty": float(product.qty_available),  # On-hand at this location
+                # "free_stock_qty": float(product.free_qty),  # Free stock (not reserved)
+                # "virtual_stock_qty": float(product.virtual_available),  # Forecasted stock
+            }
+            warehouse_data["products"].append(product_data)
+
+        location_stock_data = [warehouse_data]
+        _logger.info('location_stock_data: %s', location_stock_data)
+        _logger.info('started api calling')
+
+        update_stock = self.update_product_stock_qty_api(location_stock_data)
+        _logger.info("called update_stock Api: %s", update_stock)
+
+        return location_stock_data
+
+    def get_kit_boms_stock_as_json2(self, warehouse, stock_location):
+        _logger.info("Processing stock for warehouse: %s | location: %s", warehouse.name, stock_location.display_name)
+
+        current_company = self.company_id.id
+        kit_boms = self.env['mrp.bom'].search([
+            ('type', '=', 'phantom'),
+            ('company_id', '=', current_company)
+        ])
+        if not kit_boms:
+            raise ValueError("No BOMs with type 'kit' found")
+
+        # Only use the single affected warehouse
+        warehouse_data = {
+            "location_name": stock_location.name,
+            "parent_location_name": stock_location.location_id.name if stock_location.location_id else None,
+            "products": []
+        }
+
         for bom in kit_boms:
             available_qty = float('inf')
 
