bbbffe4bfdfcb4fd7703b54ebba7e932341bdb66
Areterix <mmalek@areterix.com>
2025-10-29 09:47:28 +0530

    Permission Issue commit after odoo upgrade the version

--- custom_sale_order/models.py
+++ custom_sale_order/models.py
@@ -956,6 +956,7 @@ class CustomerCreator(models.Model):
 
         # Create sale order
         SaleOrder = self.env['sale.order']
+        SaleOrder = self.env['sale.order'].sudo()
         new_sale_order = SaleOrder.create({
             'partner_id': existing_customer.id,
             'company_id': 1,
